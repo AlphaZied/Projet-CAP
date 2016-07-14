976f74bae7799e3418bf5709a44dfdf867db7f0e
OfficialAnti <emiledeniger@gmail.com>
2016-07-14 14:23:02 +0100

    Inscription avec Design

---
+++ inscription.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<title>ExtraGeo - Inscription</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="ExtraGeo est un jeu multijoueur en ligne et gratuit. Défiez vos amis dans des duels dont seul vos connaissances du terrain vous aideront à vous surpasser !" />
+<link rel="shortcut icon" href="/favicons.ico" />
+<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+<div class="main">
+<div class="box">
+<h3 class="box-header">Inscription</h3>
+<form class="form" method="post">
+<div class="ensemble">
+<label for="pseudo">Pseudo</label>
+<input class="form-elements" type="text" name="pseudo" id="pseudo">
+</div>
+<div class="ensemble">
+<label for="email">Email</label>
+<input class="form-elements" type="text" name="email" id="email">
+</div>
+<div class="ensemble">
+<label for="password">Mot de passe</label>
+<input class="form-elements" type="password" name="password" id="password">
+</div>
+<input type="submit" name="submit" value="S’inscrire" class="btn-submit" id="submit">
+</form>
+</div>
+<footer class="site-footer">
+<span class="site-footer-owner">
+<a href="http://topjeu.fr">ExtraGeo</a> est maintenu par <a href="https://github.com/AlphaZied">AlphaZied</a> &amp; <a href="https://github.com/OfficialAnti">OfficialAnti</a>.</span>
+</footer>
+</div>
+</body>					
+</html>
